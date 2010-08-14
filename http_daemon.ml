fd2f443c96253e3ec19e44583ed568eb415af06c
Jake Donham <jake@donham.org>
2010-08-13 23:52:13 -0400

    callbacks return a string Lwt_stream.t instead of writing to the output channel directly. probably needs work.

--- http_daemon.ml
+++ http_daemon.ml
@@ -37,11 +37,11 @@ let string_of_conn_id = string_of_int
 type daemon_spec = {
   address: string;
   auth: auth_info;
-  callback: conn_id -> Http_request.request -> Lwt_io.output_channel Lwt.t -> unit Lwt.t;
+  callback: conn_id -> Http_request.request -> string Lwt_stream.t Lwt.t;
   conn_closed : conn_id -> unit;
   port: int;
   root_dir: string option;
-  exn_handler: exn -> Lwt_io.output_channel Lwt.t -> unit Lwt.t;
+  exn_handler: exn -> unit Lwt.t;
   timeout: int option;
   auto_close: bool;
 }
@@ -62,63 +62,62 @@ let control_body code body =
 </BODY></HTML>"
     code reason_phrase code reason_phrase body
 
-let respond_with response outchan =
-  lwt outchan = outchan in
-  Http_response.serialize response outchan
+let respond_with response =
+  Lwt.return (Http_response.serialize_to_stream response)
 
   (* Warning: keep default values in sync with Http_response.response class *)
-let respond ?(body = "") ?(headers = []) ?version ?(status = `Code 200) outchan =
+let respond ?(body = "") ?(headers = []) ?version ?(status = `Code 200) () =
   let resp = Http_response.init ~body:[`String body] ~headers ?version ~status () in
-    respond_with resp outchan
+  respond_with resp
 
 let respond_control
     func_name ?(is_valid_status = fun _ -> true) ?(headers=[]) ?(body="")
-    ?version status outchan =
+    ?version status =
   let code = match status with `Code c -> c | #status as s -> code_of_status s in
   if is_valid_status code then
     let headers =
       [ "Content-Type", "text/html; charset=iso-8859-1" ] @ headers
     in
     let body = (control_body code body) ^ body in
-      respond ?version ~status ~headers ~body outchan
+      respond ?version ~status ~headers ~body ()
   else
     failwith
       (sprintf "'%d' isn't a valid status code for %s" code func_name)
       
-let respond_redirect ~location ?body ?version ?(status = `Code 301) outchan =
+let respond_redirect ~location ?body ?version ?(status = `Code 301) () =
   respond_control "Daemon.respond_redirect" ~is_valid_status:is_redirection
-    ~headers:["Location", location] ?body ?version status outchan
+    ~headers:["Location", location] ?body ?version status
 
-let respond_error ?body ?version ?(status = `Code 400) outchan =
+let respond_error ?body ?version ?(status = `Code 400) () =
   respond_control "Daemon.respond_error" ~is_valid_status:is_error
-    ?body ?version status outchan
+    ?body ?version status
     
-let respond_not_found ~url ?version outchan =
-  respond_control "Daemon.respond_not_found" ?version (`Code 404) outchan
+let respond_not_found ~url ?version () =
+  respond_control "Daemon.respond_not_found" ?version (`Code 404)
     
-let respond_forbidden ~url ?version outchan =
-  respond_control "Daemon.respond_forbidden" ?version (`Code 403) outchan
+let respond_forbidden ~url ?version () =
+  respond_control "Daemon.respond_forbidden" ?version (`Code 403)
     
-let respond_unauthorized ?version ?(realm = server_string) outchan =
+let respond_unauthorized ?version ?(realm = server_string) () =
   let body =
     sprintf "401 - Unauthorized - Authentication failed for realm \"%s\"" realm
   in
     respond ~headers:["WWW-Authenticate", sprintf "Basic realm=\"%s\"" realm]
-      ~status:(`Code 401) ~body outchan
+      ~status:(`Code 401) ~body ()
 
 let respond_file ~fname ?droot ?(version = default_version) 
-    ?(mime_type = "application/octet-stream") outchan =
+    ?(mime_type = "application/octet-stream") () =
   (** ASSUMPTION: 'fname' doesn't begin with a "/"; it's relative to the current
       document root (usually the daemon's cwd) *)
   let droot = match droot with
     |None -> Sys.getcwd () |Some d -> d in  (* document root *)
   let path = droot ^ "/" ^ fname in (* full path to the desired file *)
     if not (Sys.file_exists path) then (* file not found *)
-      respond_not_found ~url:fname outchan
+      respond_not_found ~url:fname ()
     else begin
       try
 	if Http_misc.is_directory path then (* file found, is a dir *)
-          respond_forbidden ~url:fname ~version outchan
+          respond_forbidden ~url:fname ~version ()
 	else (* file found, is something else *)
           Lwt_io.with_file ~mode:Lwt_io.input path
             (fun inchan ->
@@ -126,12 +125,12 @@ let respond_file ~fname ?droot ?(version = default_version)
                let (_, finished) = Lwt.wait () in (* don't care when file is finished *)
 	       let resp = Http_response.init ~body:[`Inchan (file_size, inchan, finished)]
 		 ~status:(`Code 200) ~version ()
-	       in respond_with resp outchan
+	       in respond_with resp
             )
       with
 	| Unix.Unix_error (Unix.EACCES, _, _)
 	| Sys_error _ ->
-            respond_forbidden ~url:fname ~version outchan
+            respond_forbidden ~url:fname ~version ()
     end
       
 (** internal: this exception is raised after a malformed request has been read
@@ -145,84 +144,98 @@ exception Again;;
   encounter a parse error (name 'Again' is intended for future versions that
   will support http keep alive signaling that a new request has to be parsed
   from client) *)
-let rec wrap_parse_request_w_safety parse_function (inchan:Lwt_io.input_channel) outchan =
-  let pp_parse_exc e =
-    sprintf "HTTP request parse error: %s" (Printexc.to_string e)
-  in catch (fun () -> parse_function inchan)
-       (function
-	  | (Malformed_request req) as e ->
-	      debug_print (pp_parse_exc e);
-	      respond_error ~status:(`Code 400)
-		~body:("request 1st line format should be: " ^
-			 "'&lt;method&gt; &lt;url&gt; &lt;version&gt;'" ^
-			 "<br />\nwhile received request 1st line was:<br />\n" ^ req)
-		outchan >>
-		fail Again
-	  | (Invalid_HTTP_method meth) as e ->
-	      debug_print (pp_parse_exc e);
-	      respond_error ~status:(`Code 501)
-		~body:("Method '" ^ meth ^ "' isn't supported (yet)")
-		outchan >>
-		fail Again
-	  | (Malformed_request_URI uri) as e ->
-	      debug_print (pp_parse_exc e);
-	      respond_error ~status:(`Code 400) ~body:("Malformed URL: '" ^ uri ^ "'")
-		outchan >>
-		fail Again
-	  | (Invalid_HTTP_version version) as e ->
-	      debug_print (pp_parse_exc e);
-	      respond_error ~status:(`Code 505)
-		~body:("HTTP version '" ^ version ^ "' isn't supported (yet)")
-		outchan >>
-		fail Again
-	  | (Malformed_query query) as e ->
-	      debug_print (pp_parse_exc e);
-	      respond_error ~status:(`Code 400)
-		~body:(sprintf "Malformed query string '%s'" query) outchan >>
-		fail Again
-	  | (Malformed_query_part (binding, query)) as e ->
-	      debug_print (pp_parse_exc e);
-	      respond_error ~status:(`Code 400)
-		~body:(sprintf "Malformed query part '%s' in query '%s'" binding query)
-		outchan >>
-		fail Again
-	  | e -> fail e)
-       
+
+let handle_parse_exn e =
+  let r =
+    match e with
+      | Malformed_request req ->
+          Some
+            (`Code 400,
+             ("request 1st line format should be: " ^
+		"'&lt;method&gt; &lt;url&gt; &lt;version&gt;'" ^
+		"<br />\nwhile received request 1st line was:<br />\n" ^ req))
+      | Invalid_HTTP_method meth ->
+          Some
+	    (`Code 501,
+             ("Method '" ^ meth ^ "' isn't supported (yet)"))
+      | Malformed_request_URI uri ->
+          Some
+            (`Code 400,
+             ("Malformed URL: '" ^ uri ^ "'"))
+      | Invalid_HTTP_version version ->
+          Some
+            (`Code 505,
+	     ("HTTP version '" ^ version ^ "' isn't supported (yet)"))
+      | Malformed_query query ->
+          Some
+            (`Code 400,
+             (sprintf "Malformed query string '%s'" query))
+      | Malformed_query_part (binding, query) ->
+	  Some
+            (`Code 400,
+             (sprintf "Malformed query part '%s' in query '%s'" binding query))
+      | _ -> None in
+
+  match r with
+    | Some (status, body) ->
+        debug_print (sprintf "HTTP request parse error: %s" (Printexc.to_string e));
+        respond_error ~status ~body ()
+    | None ->
+        fail e
+
   (* TODO what happens when a Quit exception is raised by a callback? Do other
   callbacks keep on living until the end or are they all killed immediately?
   The right semantics should obviously be the first one *)
 
   (** - handle HTTP authentication
    *  - handle automatic closures of client connections *)
-let invoke_callback conn_id (req:Http_request.request) spec (outchan:Lwt_io.output_channel Lwt.t) =
+let invoke_callback conn_id (req:Http_request.request) spec =
   try_lwt 
     (match (spec.auth, (Http_request.authorization req)) with
-       | `None, _ -> spec.callback conn_id req outchan  (* no auth required *)
+       | `None, _ -> spec.callback conn_id req (* no auth required *)
        | `Basic (realm, authfn), Some (`Basic (username, password)) ->
-	   if authfn username password then spec.callback conn_id req outchan (* auth ok *)
+	   if authfn username password then spec.callback conn_id req (* auth ok *)
 	   else fail (Unauthorized realm)
        | `Basic (realm, _), _ -> fail (Unauthorized realm)) (* auth failure *)
- with
-   | Unauthorized realm -> respond_unauthorized ~realm outchan
-   | Again -> return ()
+  with
+    | Unauthorized realm -> respond_unauthorized ~realm ()
+    | e ->
+        respond_error ~status:`Internal_server_error ~body:(Printexc.to_string e) ()
 
 let daemon_callback spec =
   let conn_id = ref 0 in
   let daemon_callback ~clisockaddr ~srvsockaddr inchan outchan =
     let conn_id = incr conn_id; !conn_id in
-    let rec loop prev =
+
+    let streams, push_streams = Lwt_stream.create () in
+    let write_streams =
+      Lwt_stream.iter_s
+        (fun stream -> stream >>= Lwt_stream.iter_s (Lwt_io.write outchan))
+        streams in
+
+    let rec loop () =
       catch (fun () -> 
         debug_print "request";
         let (finished_t, finished_u) = Lwt.wait () in
-        let outchan = prev >> Lwt.return outchan in (* wait for response to finish before writing another *)
-        lwt req = wrap_parse_request_w_safety 
-          (Http_request.init_request ~clisockaddr ~srvsockaddr finished_u) 
-          inchan outchan in
-        (* XXX Again exceptions should wakeup finished_u and loop with wakeup'd thread *)
-        debug_print "invoke_callback";
-        let prev = invoke_callback conn_id req spec outchan in
-        lwt () = finished_t in (* wait for request to finish before reading another *)
-        loop prev
+
+        let stream =
+          try_bind
+            (fun () -> Http_request.init_request ~clisockaddr ~srvsockaddr finished_u inchan)
+            (fun req ->
+               debug_print "invoke_callback";
+               invoke_callback conn_id req spec)
+            (fun e ->
+               try_bind
+                 (fun () -> handle_parse_exn e)
+                 (fun s ->
+                    Lwt.wakeup finished_u (); (* read another request *)
+                    Lwt.return s)
+                 (fun e ->
+                    Lwt.wakeup_exn finished_u e;
+                    Lwt.fail e)) in
+        push_streams (Some stream);
+
+        finished_t >>= loop (* wait for request to finish before reading another *)
       ) ( function 
          | End_of_file -> debug_print "done with connection"; spec.conn_closed conn_id; return ()
          | Canceled -> debug_print "cancelled"; spec.conn_closed conn_id; return ()
@@ -230,11 +243,12 @@ let daemon_callback spec =
     in
     debug_print "server starting";
     try_lwt
-      loop (Lwt.return ())
+      loop () <&> write_streams
     with
       | exn ->
 	  debug_print (sprintf "uncaught exception: %s" (Printexc.to_string exn));
-	  spec.exn_handler exn (Lwt.return outchan) (* XXX be sure callbacks are finished *)
+          (* XXX perhaps there should be a higher-level exn handler for 500s *)
+	  spec.exn_handler exn
   in
   daemon_callback
        
@@ -247,21 +261,21 @@ module Trivial =
   struct
     let heading_slash_RE = Pcre.regexp "^/"
 
-    let trivial_callback _ req (outchan:Lwt_io.output_channel Lwt.t) =
+    let trivial_callback _ req =
       debug_print "trivial_callback";
       let path = Http_request.path req in
       if not (Pcre.pmatch ~rex:heading_slash_RE path) then
-        respond_error ~status:(`Code 400) outchan
+        respond_error ~status:(`Code 400) ()
       else
-        respond_file ~fname:(Http_misc.strip_heading_slash path) outchan
+        respond_file ~fname:(Http_misc.strip_heading_slash path) ()
 
     let callback = trivial_callback
 
     let main spec = main { spec with callback = trivial_callback }
   end
 
-let default_callback _ _ _ = return ()
-let default_exn_handler exn _ =
+let default_callback _ _ = let (s, _) = Lwt_stream.create () in Lwt.return s
+let default_exn_handler exn =
   debug_print "no handler given: re-raising";
   fail exn
 
