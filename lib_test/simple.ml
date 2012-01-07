a6b323624cf12c7e6ce70b1b30a149f5e743dd20
Anil Madhavapeddy <anil@recoil.org>
2012-01-07 20:52:28 +0000

    modify test to act as a client and server

--- lib_test/simple.ml
+++ lib_test/simple.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2010-2011 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2010-2012 Anil Madhavapeddy <anil@recoil.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,15 +14,30 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-let _ =
+open Lwt
+open Printf
 
-  Lwt_main.run ( 
-    let port = 8080 in
-    let spec = {
-      Cohttpd.Server.default_spec with
-        Cohttpd.Server.callback = Dispatch.t;
-        port = port
-    } 
-    in
-    Cohttpd.Server.main spec
-  )
+let server_t =
+  let open Cohttpd.Server in
+  let port = 8080 in
+  let spec = { default_spec with callback=Dispatch.t; port=port; auto_close=true } in
+  main spec
+
+let cancel_t =
+  lwt () = Lwt_unix.sleep 10.0 in
+  exit 0
+
+let req_num = ref 0
+
+let rec request_t () =
+  lwt () = Lwt_unix.sleep 1.0 in
+  incr req_num;
+  let uri = Uri.of_string "http://localhost:8080/" in
+  lwt hdrs, body = Cohttpd.Client.get uri in
+  printf "#%d:\n%!" !req_num;
+  List.iter (fun (k,v) -> printf "  %.20s: %s\n" k v) hdrs;
+  printf "%s\n\n%!" body;
+  request_t ()
+
+let _ =
+  Lwt_main.run (server_t <?> cancel_t <?> (request_t ()))
